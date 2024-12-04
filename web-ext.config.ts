dfcae12e93a39ea8b217f74f6ffda918e5083067
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-12-03 21:04:57 -0330

    feat: Specify chrome canary binary

---
+++ web-ext.config.ts
@@ -0,0 +1,7 @@
+import { defineRunnerConfig } from 'wxt';
+
+export default defineRunnerConfig({
+  binaries: {
+    chrome: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary', // Use Chrome Beta instead of regular Chrome
+  },
+});
